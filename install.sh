2e896f8c75cc9587318427b671608af612ec5dd6
Brian DeHamer <brian@dehamer.com>
2015-07-31 22:56:53 +0000

    Add default gitconfig

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ echo "Linking dot files into $HOME"
 ln -s $(pwd)/bash_aliases $HOME/.bash_aliases
 ln -s $(pwd)/editrc $HOME/.editrc
 ln -s $(pwd)/gemrc $HOME/.gemrc
+ln -s $(pwd)/gitconfig $HOME/.gitconfig
+ln -s $(pwd)/inputrc $HOME/.inputrc
 ln -s $(pwd)/inputrc $HOME/.inputrc
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/vim $HOME/.vim
